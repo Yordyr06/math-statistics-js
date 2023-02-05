b3eba8a9005f1212e3e208e06b864f3eda166332
Yordy <yoalmonte.dev@outlook.com>
2023-02-05 03:49:28 -0400

    Sexta y septima clase: Reto aplicacion de cupones de descuento. Primer vistaso a los metodos de los arrys

--- discount/discount.js
+++ discount/discount.js
@@ -1,25 +1,35 @@
 const inputPrice = document.getElementById('price');
-const inputDiscount = document.getElementById('discount');
+const inputCoupon = document.getElementById('coupon');
 const btn = document.getElementById('calc');
 const result = document.getElementById('result');
+
+const coupon = {
+  'spring' : 10,
+  'summer' : 15,
+  'autumn' : 20,
+  'winter' : 25,
+}
 
 btn.addEventListener('click', discountCalc);
 
 function discountCalc() {
   const price = Number(inputPrice.value);
-  const discount = Number(inputDiscount.value);
-  const newPrice = (price * (100 - discount) / 100);
+  let discount;
 
-  if (!price || !discount) {
-    result.innerText = 'Favor llenar todos los campos';
-    return;
-  }
-  
-  if (discount > 100) {
-    result.innerText = 'Descuento errado';
-    return;
-  }
+  inputCoupon.value = inputCoupon.value.toLowerCase();
 
-  result.innerText = 'Nuevo precio: $' + newPrice;
-  console.log('Proceso exitoso');
+  if (price != 0 && Object.getOwnPropertyNames(coupon).includes(inputCoupon.value)) {
+    discount = coupon[inputCoupon.value];
+    const newPrice = (price * (100 - discount) / 100);
+    result.innerText = `Nuevo precio: $${newPrice}`;
+    console.log(`Proceso exitoso`);
+    return
+
+  } else if (Object.getOwnPropertyNames(coupon).includes(inputCoupon.value) == false) {
+    result.innerText = `Cupón incorrecto, prueba con otro cupón`
+    return
+
+  } else {
+    result.innerText = `Has ingresado un valor errado`
+  }
 }
