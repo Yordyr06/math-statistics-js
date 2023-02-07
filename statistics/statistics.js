36394390c04aa581b6f4025e82362aed96a48428
Yordy <yoalmonte.dev@outlook.com>
2023-02-07 03:13:07 -0400

    Undecima, duodecima y decimotercera clase: Metodo Array.Prototype.Sort(), reto de funcion median

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -1,6 +1,20 @@
 // Funcion para calcular promedio
-
 const average = (array) => {
   const result = array.reduce((sum, no) => sum + no) / array.length;
   return result;
+}
+
+// Fucion para determinar la mediana
+const median = (array) => {
+  let median;
+  array = array.sort((a, b) => a - b);
+  
+  if (!(array.length % 2)) {
+    median = array.length / 2;
+    return average([array[median, median - 1]]);
+
+  } else {
+    median = Math.floor(array.length / 2);
+    return array[median];
+  }
 }
