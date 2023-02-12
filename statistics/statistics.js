f3379c92fab18f5efbc54a66bb8dc99c26a582ab
Yordy <yoalmonte.dev@outlook.com>
2023-02-12 06:02:53 -0400

    Reto: Calculando otro tipo de cosas. Promedio Ponderado

--- statistics/statistics.js
+++ statistics/statistics.js
@@ -37,3 +37,47 @@ const mode = (array) => {
   mode = arr[0];
   return mode;
 };
+
+// Reto. Calcular otro tipo de promedio.
+  // ¿Cuál tipo de promedio elegiste para trabajar?
+  // ¿Qué casos de uso tiene tu tipo de promedio?
+  // ¿Cómo traduces su fórmula a código JavaScript?
+
+// Promedio Ponderado.
+// Definir el conjunto de números junto al peso de cada elemento
+const notes = [
+  {
+    subject: 'Programming',
+    note: 10,
+    credit: 4,
+  },
+
+  {
+    subject: 'Data Base',
+    note: 7,
+    credit: 3,
+  },
+  
+  {
+    subject: 'Cloud',
+    note: 8,
+    credit: 5,
+  },
+];
+
+const weightedAverage = (array) => {
+  // Multiplicar cada número de la lista por su peso
+  let subject = array.map(value => value.note * value.credit);
+
+  // Sumar todos los elementos del arreglo de elementos multiplicados por su peso
+  subject = subject.reduce((sum, value) => sum + value);
+
+  // Sumar todos los pesos (créditos)
+  let credit = array.map(value => value.credit);
+  credit = credit.reduce((sum, value) => sum + value);
+
+  // Hacer la división entre ambas “sumas”
+  let result = subject / credit;
+  result = Number(result.toFixed(2));
+  return result;
+}
